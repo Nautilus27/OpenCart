d393da2c15773adff867896d02be62e67a5703c5
Nautilus27 <34571786+Nautilus27@users.noreply.github.com>
2017-12-26 20:59:29 +0200

    First
    first

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev1.dreamvention.com/admin/');
+define('HTTP_CATALOG', 'http://dev1.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev1.dreamvention.com/admin/');
+define('HTTPS_CATALOG', 'http://dev1.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/dev1/public_html/admin/');
+define('DIR_SYSTEM', '/var/www/dev1/public_html/system/');
+define('DIR_LANGUAGE', '/var/www/dev1/public_html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/dev1/public_html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/dev1/public_html/system/config/');
+define('DIR_IMAGE', '/var/www/dev1/public_html/image/');
+define('DIR_CACHE', '/var/www/dev1/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev1/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/dev1/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dev1/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dev1/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/dev1/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dev1_id108');
+define('DB_PASSWORD', 'tVWrpW6l');
+define('DB_DATABASE', 'dev1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev1.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev1.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/dev1/public_html/catalog/');
+define('DIR_SYSTEM', '/var/www/dev1/public_html/system/');
+define('DIR_LANGUAGE', '/var/www/dev1/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/dev1/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/dev1/public_html/system/config/');
+define('DIR_IMAGE', '/var/www/dev1/public_html/image/');
+define('DIR_CACHE', '/var/www/dev1/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev1/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/dev1/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dev1/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dev1/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dev1_id108');
+define('DB_PASSWORD', 'tVWrpW6l');
+define('DB_DATABASE', 'dev1');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
